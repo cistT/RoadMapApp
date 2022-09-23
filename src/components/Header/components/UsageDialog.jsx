8b344cc46d1e3d2051780e3fcc9152db3a57b6df
rez13 <cist.b220.y.katou@gmail.com>
2022-09-24 01:06:37 +0900

    使い方を削除

--- src/components/Header/components/UsageDialog.jsx
+++ src/components/Header/components/UsageDialog.jsx
@@ -1,33 +1,33 @@
-import Dialog from "@mui/material/Dialog";
-import DialogActions from "@mui/material/DialogActions";
-import DialogContent from "@mui/material/DialogContent";
-import DialogTitle from "@mui/material/DialogTitle";
-import UsageButton from "../../Button/UsageButton";
-import BackButton from "../../Button/BackButton";
+// import Dialog from "@mui/material/Dialog";
+// import DialogActions from "@mui/material/DialogActions";
+// import DialogContent from "@mui/material/DialogContent";
+// import DialogTitle from "@mui/material/DialogTitle";
+// import UsageButton from "../../Button/UsageButton";
+// import BackButton from "../../Button/BackButton";
 
-import useDialog from "hooks/useDialog";
+// import useDialog from "hooks/useDialog";
 
-const UsageDialog = () => {
-    const { open, handleOpen, handleClose } = useDialog(false);
-    return (
-        <>
-            <UsageButton onClick={handleOpen} />
+// const UsageDialog = () => {
+//     const { open, handleOpen, handleClose } = useDialog(false);
+//     return (
+//         <>
+//             <UsageButton onClick={handleOpen} />
 
-            <Dialog open={open} onClose={handleClose}>
-                <DialogTitle>使い方</DialogTitle>
+//             <Dialog open={open} onClose={handleClose}>
+//                 <DialogTitle>使い方</DialogTitle>
 
-                <DialogContent
-                    style={{ display: "flex", whiteSpace: "pre-line" }}
-                >
-                    <div style={{ width: "40vw" }}>制作中</div>
-                </DialogContent>
+//                 <DialogContent
+//                     style={{ display: "flex", whiteSpace: "pre-line" }}
+//                 >
+//                     <div style={{ width: "40vw" }}>制作中</div>
+//                 </DialogContent>
 
-                <DialogActions>
-                    <BackButton onClick={handleClose} />
-                </DialogActions>
-            </Dialog>
-        </>
-    );
-};
+//                 <DialogActions>
+//                     <BackButton onClick={handleClose} />
+//                 </DialogActions>
+//             </Dialog>
+//         </>
+//     );
+// };
 
-export default UsageDialog;
+// export default UsageDialog;
